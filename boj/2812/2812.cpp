d1797b99274dc8312c612797cff748a7e3f54587
Lee-Jiseung <ljss98@naver.com>
2022-02-04 16:56:26 +0900

    week1 크게 만들기

--- boj/2812/2812.cpp
+++ boj/2812/2812.cpp
@@ -10,38 +10,26 @@ using namespace std;
 
 int n, k;
 string s;
+deque<char> d;
 
-void makeBigger(int start, int count) {
-	if (s.size() == n - k) return;
-
-	char large = s[start];
-	int index = start;
-	for (int i = start; i <= k - count + start; i++) {
-		//cout << i<<' '<<count<<' '<<start<<' '<< k - count + start<<'\n';
-		if (large < s[i]) {
-			large = s[i];
-			index = i;
-			break;
-		}
-	}
-
-	if (index > start) {
-		s.erase(start, 1);
-		makeBigger(start, count + 1);
-	}
-	else {
-		makeBigger(start + 1, count);
-	}
-
-}
 
 int main() {
 
 	cin >> n >> k;
 	cin >> s;
 
-	makeBigger(0, 0);
+	
 
-	cout << s;
+	for (int i = 0; i < n; i++) {
+		while (!d.empty() && k > 0 && d.back() < s[i]) {
+			d.pop_back();
+			k--;
+		}
+		d.push_back(s[i]);
+	}
+
+	for (int i = 0; i < (d.size()-k); i++) {
+		cout << d[i];
+	}
 	return 0;
 }
