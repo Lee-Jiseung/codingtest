4be5bee97da56a66b84a3de3150b54c535a39319
Lee-Jiseung <ljss98@naver.com>
2022-04-04 14:46:07 +0900

    20220402

--- boj/week10/wanted3.cpp
+++ boj/week10/wanted3.cpp
@@ -40,61 +40,7 @@ int main() {
     long long answer = 0;
     for (int i = 0; i < v.size(); i++) {
         if (v[i] < 0 && countE >= 2) {
-            cout << v[i] << ' ' << countE<<'\n';
-            int temp = (dp[countE] + 1000000007 - countE - 1) % 1000000007;#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <deque>
-
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    int n;
-    cin >> n;
-
-    vector<int> v;
-    int countW = 0;
-    int countE = 0;
-    for (int i = 0; i < n; i++) {
-        char c;
-        cin >> c;
-        if (c == 'W') {
-            countW++;
-        }
-        else if (c == 'H') {
-            v.push_back(countW);
-        }
-        else if (c == 'E') {
-            countE++;
-            v.push_back(0);
-        }
-    }
-
-    vector<int> dp(v.size(), 0);
-    dp[1] = 2;
-    for (int i = 2; i < dp.size(); i++) {
-        dp[i] = (dp[i - 1] * 2) % 1000000007;
-    }
-    
-    long long answer = 0;
-    for (int i = 0; i < v.size(); i++) {
-        if (v[i] > 0 && countE >= 2) {
-            cout << v[i] << ' ' << countE<<'\n';
             int temp = (dp[countE] + 1000000007 - countE - 1) % 1000000007;
-            answer = (answer + (long long) v[i] * -1 * temp) % 1000000007;
-        }
-        else if (v[i] == 2) {
-            countE--;
-        }
-
-    }
-
-    cout << answer;
-    return 0;
-}
             answer = (answer + (long long) v[i] * -1 * temp) % 1000000007;
         }
         else if (v[i] == 2) {
